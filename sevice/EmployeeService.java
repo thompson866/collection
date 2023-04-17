94d32a9f99864967e87c3345ae295d3ae72ebf74
arxip <arxipov1989@gmail.com>
2023-04-17 23:36:43 +0300

    no spring

--- sevice/EmployeeService.java
+++ sevice/EmployeeService.java
@@ -6,38 +6,35 @@ import pro.sky.com.example.employeeworkdemo.exception.EmployeeStorageIsFullExcep
 import pro.sky.com.example.employeeworkdemo.exception.EmployeeNotFoundException;
 import pro.sky.com.example.employeeworkdemo.model.Employee;
 
+import javax.annotation.PostConstruct;
+
 @Service
 public class EmployeeService {
 
-    private static final int SIZE =4;
+    private static final int SIZE = 4;
     private final Employee[] employee = new Employee[SIZE];
+
+    @PostConstruct
+    public void init() {
+        employee[0] = new Employee("Вуди", "Вудпекер");
+    }
 
     public Employee addEmployee(String firstName, String lastName) {
         int startEmp = -1;
         Employee employeeNew = new Employee(firstName, lastName);
         for (int i = 0; i < employee.length; i++) {
-            if (employeeNew == null && startEmp == -1) {
+            if (employee[i] == null && startEmp == -1) {
                 startEmp = i;
             }
-            if (employee[i] == null && employee[i].equals(employeeNew)) {
+            if (employee[i] != null && employee[i].equals(employeeNew)) {
                 throw new EmployeeAlreadyAddedException();
             }
         }
-        if (startEmp != -1) {
+        if (startEmp == -1) {
             throw new EmployeeStorageIsFullException();
         }
         employee[startEmp] = employeeNew;
         return employee[startEmp];
-    }
-
-    public Employee findEmployee(String firstName, String lastName) {
-        Employee employeeNew = new Employee(firstName, lastName);
-        for (Employee employee : employee) {
-            if (employee.equals(employeeNew)) {
-                return employeeNew;
-            }
-        }
-        throw new EmployeeNotFoundException();
     }
 
     public Employee removeEmployee(String firstName, String lastName) {
@@ -51,4 +48,16 @@ public class EmployeeService {
         }
         throw new EmployeeNotFoundException();
     }
+
+    public Employee findEmployee(String firstName, String lastName) {
+        Employee employeeNew = new Employee(firstName, lastName);
+        for (Employee employee : employee) {
+            if (employee.equals(employeeNew)) {
+                return employeeNew;
+            }
+        }
+        throw new EmployeeNotFoundException();
+    }
+
+
 }
