2b188fd7826f3f90482b1183b4f7250f891efc1f
arxip <arxipov1989@gmail.com>
2023-04-19 21:32:10 +0300

    newWork newBranch

--- model/Employee.java
+++ model/Employee.java
@@ -6,69 +6,69 @@ import java.util.Objects;
 
 public class Employee {
 
-  @JsonProperty("firstName")
-  private final String name;
+    @JsonProperty("firstName")
+    private final String name;
 
-  @JsonProperty("lastName")
-  private final String surname;
+    @JsonProperty("lastName")
+    private final String surname;
 
-  private int department;
-  private int salary;
+    private int department;
+    private int salary;
 
-  public Employee(String name,
-                  String surname,
-                  int department,
-                  int salary) {
-    this.name = name;
-    this.surname = surname;
-    this.department = department;
-    this.salary = salary;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public String getSurname() {
-    return surname;
-  }
-
-  public int getDepartment() {
-    return department;
-  }
-
-  public void setDepartment(int department) {
-    this.department = department;
-  }
-
-  public int getSalary() {
-    return salary;
-  }
-
-  public void setSalary(int salary) {
-    this.salary = salary;
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
+    public Employee(String name,
+                    String surname,
+                    int department,
+                    int salary) {
+        this.name = name;
+        this.surname = surname;
+        this.department = department;
+        this.salary = salary;
     }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
+
+    public String getName() {
+        return name;
     }
-    Employee employee = (Employee) o;
-    return Objects.equals(name, employee.name) && Objects.equals(surname, employee.surname);
-  }
 
-  @Override
-  public int hashCode() {
-    return Objects.hash(name, surname);
-  }
+    public String getSurname() {
+        return surname;
+    }
 
-  @Override
-  public String toString() {
-    return String.format("ФИ: %s %s, отдел: %d, ЗП: %d", surname, name, department, salary);
-  }
+    public int getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Employee employee = (Employee) o;
+        return Objects.equals(name, employee.name) && Objects.equals(surname, employee.surname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Имя " + surname + " " + name + " отдел:" + department + " зарплата: " + salary);
+    }
 
 }
